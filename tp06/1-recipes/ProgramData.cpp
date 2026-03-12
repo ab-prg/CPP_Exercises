a5181fd5fc48e7152c2a3866b0e0bc4d911c3d39
ab-prg <ton_email_github@example.com>
2026-03-12 12:50:00 +0100

    Sauvegarde de mon travail sur le TP06

--- tp06/1-recipes/ProgramData.cpp
+++ tp06/1-recipes/ProgramData.cpp
@@ -7,10 +7,11 @@ void ProgramData::add_material(std::string name)
 void ProgramData::get_materials(std::vector<const Material*>& materials) const
 {}
 
-void ProgramData::register_recipe(std::vector<std::string> materials, std::vector<std::string> products)
+void ProgramData::register_recipe(std::vector<std::string> materials, 
+    std::vector<std::string> products)
 {}
 
-void ProgramData::collect_doable_recipes(std::vector<const Recipe*>& recipes) const
+void ProgramData::collect_doable_recipes(std::vector<const Recipe*>& recipes)  const
 {}
 
 ProductionResult ProgramData::produce(size_t recipe_id)
